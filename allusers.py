e9e5b9b6980ce789fcbfe9cf80704883d0a60e2f
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-02-23 19:47:10 +0100

    allusers2 -> allusers

--- allusers.py
+++ allusers.py
@@ -1,16 +1,17 @@
 from urllib import urlopen
 import json
+import os
 
 #
 # Gets all users who participated in the Google Code Jam competition.
-# Posts results in a single text file.
+# Posts results according to round number.
 #
 
-users = {} # dictionary of all discovered users
-
-# adds all users who participated in the given round to the dictionary
+# writes a list of all users who participated in the round
 def get_all_users(round_id, num_players):
-	global users
+	round_file = open('users/' + round_id + '.txt', 'w')
+
+	# loop through each page of users
 	for pos in range(1, int(num_players), 30):
 		meta_url = "http://code.google.com/codejam/contest/scoreboard/" \
 			+ "do?cmd=GetScoreboard&contest_id=" \
@@ -18,25 +19,25 @@ def get_all_users(round_id, num_players):
 			+ "&show_type=all&start_pos=" \
 			+ str(pos) \
 			+ "&views_time=1&views_file=0&csrfmiddlewaretoken="
+		print meta_url
 		meta_url_data = urlopen(meta_url).read()
 		meta_json = json.loads(meta_url_data)
+
+		# find and print usernames
 		for row in meta_json['rows']:
 			username = row['n']
-			users[username] = True
+			round_file.write(username)
+			round_file.write('\n')
+			print username
+	round_file.close()
 
-user_file = open('users/users.txt', 'w')
+# load JSON
 metadatafile = open(os.path.dirname(os.path.realpath(__file__)) + "/CodeJamMetadata.json").read()
 metadata = json.loads(metadatafile)
 
 # loop through all years
 for year_json in metadata['competitions']:
-	qual_round = year_json['round'][0] # get only the qualification round
-	num_players = qual_round['numPlayers']
-	round_id = qual_round['contest']
-	get_all_users(round_id, num_players) # get users for the qualification round of the given year
-
-# write out all users
-for user in users.keys():
-	user_file.write(user)
-	user_file.write('\n')
-user_file.close()
+	for round_json in year_json['round']:
+		num_players = round_json['numPlayers']
+		round_id = round_json['contest']
+		get_all_users(round_id, num_players)
