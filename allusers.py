9c441bd4ac2274cb55a9bbd0312f3f81968b6c94
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-02-23 20:26:36 +0100

    Consistent whitespace

--- allusers.py
+++ allusers.py
@@ -9,27 +9,27 @@ import os
 
 # writes a list of all users who participated in the round
 def get_all_users(round_id, num_players):
-	round_file = open('users/' + round_id + '.txt', 'w')
+    round_file = open('users/' + round_id + '.txt', 'w')
 
-	# loop through each page of users
-	for pos in range(1, int(num_players), 30):
-		meta_url = "http://code.google.com/codejam/contest/scoreboard/" \
-			+ "do?cmd=GetScoreboard&contest_id=" \
-			+ round_id \
-			+ "&show_type=all&start_pos=" \
-			+ str(pos) \
-			+ "&views_time=1&views_file=0&csrfmiddlewaretoken="
-		print meta_url
-		meta_url_data = urlopen(meta_url).read()
-		meta_json = json.loads(meta_url_data)
+    # loop through each page of users
+    for pos in range(1, int(num_players), 30):
+        meta_url = "http://code.google.com/codejam/contest/scoreboard/" \
+            + "do?cmd=GetScoreboard&contest_id=" \
+            + round_id \
+            + "&show_type=all&start_pos=" \
+            + str(pos) \
+            + "&views_time=1&views_file=0&csrfmiddlewaretoken="
+        print meta_url
+        meta_url_data = urlopen(meta_url).read()
+        meta_json = json.loads(meta_url_data)
 
-		# find and print usernames
-		for row in meta_json['rows']:
-			username = row['n']
-			round_file.write(username)
-			round_file.write('\n')
-			print username
-	round_file.close()
+        # find and print usernames
+        for row in meta_json['rows']:
+            username = row['n']
+            round_file.write(username)
+            round_file.write('\n')
+            print username
+    round_file.close()
 
 # load JSON
 metadatafile = open(os.path.dirname(os.path.realpath(__file__)) + "/CodeJamMetadata.json").read()
@@ -37,7 +37,7 @@ metadata = json.loads(metadatafile)
 
 # loop through all years
 for year_json in metadata['competitions']:
-	for round_json in year_json['round']:
-		num_players = round_json['numPlayers']
-		round_id = round_json['contest']
-		get_all_users(round_id, num_players)
+    for round_json in year_json['round']:
+        num_players = round_json['numPlayers']
+        round_id = round_json['contest']
+        get_all_users(round_id, num_players)
--- compile.py
+++ compile.py
@@ -13,30 +13,30 @@ flags.pop(0)
 
 # go through all files under the root directory
 for (path, dirs, files) in os.walk('.'):
-	for f in files:
-		# check if file is a C or C++ file
-		if f.endswith(('.c', '.cpp')):
-			if f.endswith('.c'): # C file
-				command = "gcc "
-			else: # C++ file
-				command = "g++ "
+    for f in files:
+        # check if file is a C or C++ file
+        if f.endswith(('.c', '.cpp')):
+            if f.endswith('.c'): # C file
+                command = "gcc "
+            else: # C++ file
+                command = "g++ "
 
-			# add full directory path of the source file
-			command += path
-			command += '/'
-			command += f
+            # add full directory path of the source file
+            command += path
+            command += '/'
+            command += f
 
-			# add full directory path of the executable
-			command += " -o "
-			command += path
-			command += '/'
-			command += os.path.splitext(f)[0] # remove file extension
+            # add full directory path of the executable
+            command += " -o "
+            command += path
+            command += '/'
+            command += os.path.splitext(f)[0] # remove file extension
 
-			# adding in compiler flags (specified in the arguments)
-			for flag in flags:
-				command += " "
-				command += flag
+            # adding in compiler flags (specified in the arguments)
+            for flag in flags:
+                command += " "
+                command += flag
 
-			# compile
-			os.system(command)
-			print command
+            # compile
+            os.system(command)
+            print command
