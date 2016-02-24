275bcca47ea89d55e653d9fb2cb2ebca97110b00
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-02-24 11:55:56 +0100

    Perform unzipping in memory

--- scrape.py
+++ scrape.py
@@ -6,13 +6,15 @@ import os
 import zipfile
 import shutil
 import multiprocessing
+import requests
+import StringIO
 
 #
 # Scrapes Google Code Jam data, and extracts the C/C++/Python source files.
 #
 # The directory structure and naming convention of the data is as follows:
 #
-# ./codejamdata/ |--> c/   | --> username0 | --> p[problem_number].[user_name]0.c
+# ./solutions/   |--> c/   | --> username0 | --> p[problem_number].[user_name]0.c
 #                |         |               | --> p2453486.Bob0.c
 #                |         |               | --> etc...
 #                |         |
@@ -39,7 +41,9 @@ def get_download_url(round_id, problem_id, username):
                 + username
 
 # scrapes the C/C++/Python files of the given round
-def scrape(round_id, problems, script_path):
+def scrape(round_id, round_desc, problems, script_path):
+    totalErrors = 0
+    print "START: {}".format(round_desc)
 
     # load list of users
     user_file = open(script_path + '/users/' + round_id + '.txt', 'r')
@@ -51,25 +55,13 @@ def scrape(round_id, problems, script_path):
 
         # loop through users who participated in the round
         for username in users:
-            download_url = get_download_url(round_id, problem_id, username)
-
-            # print and flush URL
-            print download_url
-            sys.stdout.flush()
-            
-            # make temp directory for storing zips
-            tempdir = round_id + 'temp'
-            if not os.path.exists(tempdir):
-                os.makedirs(tempdir)
 
             # download and read zip
-            target_zip = tempdir + '/' + problem_id + '.' + username + '0.zip'
-            urlretrieve(download_url,target_zip)
-            zip_header = open(target_zip, 'rb')
-
             # try-except in case of a bad header
             try:
-                my_zip = zipfile.ZipFile(zip_header)
+                download_url = get_download_url(round_id, problem_id, username)
+                request = requests.get(download_url, stream=True)
+                my_zip = zipfile.ZipFile(StringIO.StringIO(request.content))
 
                 # loop through each file in the zip file
                 for my_file in my_zip.namelist():
@@ -84,27 +76,21 @@ def scrape(round_id, problems, script_path):
                         elif my_file.endswith('.cpp'):
                             file_newname += 'cpp'
                             target_source = 'cpp/' + target_source
-                        target_source = 'codejamfolder/' + target_source
+                        target_source = 'solutions/' + target_source
 
                         # make directory for language and author
                         if not os.path.exists(target_source):
                             os.makedirs(target_source)
 
                         # extract and rename source file
-                        my_zip.extract(my_file, target_source)
-                        os.rename((target_source + '/' + my_file), (target_source + '/' + file_newname))
+                        f = open(os.path.join(target_source, file_newname), 'wb')
+                        f.write(my_zip.read(my_file))
+                        f.close()
                         
-                        # print location of extracted source file
-                        print target_source + '/' + file_newname
-                        sys.stdout.flush()
             except:
-                print "error:", sys.exc_info()[0] # can happen if the user didn't do a problem
-                sys.stdout.flush()
+                totalErrors += 1
 
-            # delete temp directory
-            if os.path.exists(tempdir):
-                shutil.rmtree(tempdir)
-    return
+    print "DONE: {} (Errors: {})".format(year, round_desc, totalErrors)
 
 # main section of script
 if __name__ == '__main__':
@@ -114,13 +100,13 @@ if __name__ == '__main__':
 
     # loop through years
     for year_json in metadata['competitions']:
-        year = year_json['year']
 
         # loop through rounds
         for round_json in year_json['round']:
             round_id = round_json['contest']
+            round_desc = round_json['desc']
             problems = round_json['problems']
 
             # run scraper on current round
-            scraper = multiprocessing.Process(target=scrape, args=(round_id, problems, script_path))
+            scraper = multiprocessing.Process(target=scrape, args=(round_id, round_desc, problems, script_path))
             scraper.start()
